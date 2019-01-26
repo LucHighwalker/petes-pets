9af29e1681e50ec21c32c56eda987e05783b8c2b
LucHighwalker <luc.highwalker@gmail.com>
2019-01-26 12:36:23 -0800

    cloned starter and added comments.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ if (!process.env.PORT) {
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+// morgan is new to me and it logs http requests.
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
